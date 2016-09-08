db4091548144496df427f2e4fe4dcaccd8add52a
mrinspire <lee.white@email.cerrocoso.edu>
2016-09-08 14:08:18 -0700

    Faithful Fruit Factory Website
    This is my Faithful Fruit Factory website folder (last name of folder is
    white)

---
+++ index-01.php
@@ -0,0 +1,9 @@
+
+<h1>Advanced Web Development</h1>
+
+
+<p>DMA C119 Fall 2016</p>
+<p>CRN 72233</p>
+<p><strong>Instructor’s Name:</strong> Hello Elaine M. Rudis Jackson</p>
+<p><strong>Student’s Name:</strong> Lee White</p>
+<p><strong>Email:</strong> lee.white@email.cerrocoso.edu</p>
