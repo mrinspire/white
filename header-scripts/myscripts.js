87990d7f50fa0bf7acd54c8d369af0fbaf63c597
mrinspire <lee.white@email.cerrocoso.edu>
2016-09-24 15:25:04 -0700

    Add files via upload

--- header-scripts/myscripts.js
+++ header-scripts/myscripts.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+/* Start Auto Focus fallback for all browsers */
+
+    $(document).bind('autofocus_ready', function() {
+      if (!("autofocus" in document.createElement("input"))) {
+        $("#firstname").focus();
+      }
+    });
+/* 
+Note: The rest of the script to trigger it if needed is in my footer scripts
+($(document).trigger('autofocus_ready');)  
+*/
+
+/* End Auto Focus fallback for all browsers */
